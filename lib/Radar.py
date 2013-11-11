af9895878b9bf84f31490bc748b1b6f93a5121a4
Tony Baker <anthony.s.baker@gmail.com>
2013-11-10 22:20:44 -0500

    Adding blob detection and motion

--- lib/Radar.py
+++ lib/Radar.py
@@ -1,4 +1,6 @@
 from RadarColor import *
+from SimpleCV import *
+import numpy as np
 
 class Radar:
   def __init__(self, image):
@@ -16,7 +18,45 @@ class Radar:
   def findRain(self):
     return self.removeReflectivity(RadarColor.Z0, RadarColor.Z5, RadarColor.Z10, RadarColor.Z15, RadarColor.Z20)
 
-
   def smooth(self):
     return Radar(self.image.copy().smooth())
+
+  def placePoi(self, point):
+    copy = self.image.copy()
+    copy.drawCircle(point, 2, Color.FUCHSIA, thickness=-1)
+    return Radar(copy)
+
+  def show(self):
+    self.image.show()
+
+  def waitForClick(self, disp):
+    while disp.isNotDone():
+      if disp.mouseLeft:
+        break
+
+  def getCells(self):
+    copy = self.image.copy()
+
+    palette = copy.getPalette(bins=2)
+    palette = np.array(filter(lambda c: c[0] < 200, palette))
+
+    blobs = copy.findBlobsFromPalette(palette)
+
+    if (blobs is not None):
+      print "wooooooooo"
+      for b in blobs:
+        b.drawOutline(color=Color.LIME, width=-1,alpha=128)
+
+    return (Radar(copy), blobs)
+
+  def getMotion(self, prior):
+    copy = self.image.copy()
+
+    motion = copy.findMotion(prior.image, method="HS")
+    filtered_motion = filter(lambda m: m.magnitude() > 0 and copy.getPixel(int(m.x), int(m.y)) != Color.WHITE, motion)
+
+    for m in filtered_motion:
+      m.draw(color=Color.PLUM,normalize=False)
+
+    return (Radar(copy), filtered_motion)
 
--- segment.py
+++ segment.py
@@ -2,10 +2,10 @@ from lib.Radar import *
 from lib.RadarColor import *
 from SimpleCV import *
 
-disp = Display()
 
 range_px = 578
 range_miles = 142
+disp = Display()
 
 
 # sample-data
@@ -13,52 +13,48 @@ range_miles = 142
 #img2 =  Image("sample_data/DTX_20131021_2302_NCR.gif")
 
 # heavy started at 13:34
-
-img1 = Image("sample_data/DTX_20131106_1327_NCR.gif")
-img2 = Image("sample_data/DTX_20131106_1333_NCR.gif")
+img1 = Image("sample_data/DTX_20131106_1223_NCR.gif")
+img2 = Image("sample_data/DTX_20131106_1229_NCR.gif")
+img3 = Image("sample_data/DTX_20131106_1235_NCR.gif")
+#img = Image("sample_data/DTX_20131106_1241_NCR.gif")
+#img = Image("sample_data/DTX_20131106_1247_NCR.gif")
+#img = Image("sample_data/DTX_20131106_1252_NCR.gif")
+#img = Image("sample_data/DTX_20131106_1258_NCR.gif")
+#img = Image("sample_data/DTX_20131106_1304_NCR.gif")
+#img = Image("sample_data/DTX_20131106_1310_NCR.gif")
+#img = Image("sample_data/DTX_20131106_1316_NCR.gif")
+#img = Image("sample_data/DTX_20131106_1321_NCR.gif")
+#img = Image("sample_data/DTX_20131106_1327_NCR.gif")
+#img1 = Image("sample_data/DTX_20131106_1333_NCR.gif")
+#img2 = Image("sample_data/DTX_20131106_1339_NCR.gif")
 
 radar1 = Radar(img1.copy())
-radar2 = Radar(img2.copy())
-
 rain1 = radar1.findRain().smooth();
-rain1.image.drawCircle( (350,290), 2, Color.FUCHSIA, thickness=-1)
+rain1 = rain1.placePoi((350,290))
+(rain1, rain1_blobs) = rain1.getCells()
 
+radar2 = Radar(img2.copy())
 rain2 = radar2.findRain().smooth();
-rain2.image.drawCircle( (350,290), 2, Color.FUCHSIA, thickness=-1)
+rain2 = rain2.placePoi((350,290))
+#(rain2, rain2_blobs) = rain2.getCells()
+(rain2, rain2_motion) = rain2.getMotion(rain1)
+
+radar3 = Radar(img3.copy())
+rain3 = radar3.findRain().smooth();
+rain3 = rain3.placePoi((350,290))
 
 
 
-motion = rain2.image.findMotion(rain1.image, method="HS")
-
-mags = []
-
-for m in motion:
-  if m.magnitude() > 0 and rain2.image.getPixel(int(m.x), int(m.y)) != Color.WHITE:
-    print "x:", m.x, "y:", m.y, "mag:", m.magnitude(), "vec:", m.vector(), rain2.image.getPixel(int(m.x),int(m.y))
-    mags.append(m.magnitude())
-    m.draw(color=Color.RED,normalize=False)
-
-#rain1.image.show();
-#raw_input("press any key....")
-rain2.image.show();
-
-
-average = np.average(mags)
-maximum = np.amax(mags)
-
-print "average px: " + str(average)
-print "average miles: " + str(average*142/578*4)
-
-print "largest px: " + str(maximum)
-print "largest miles: " + str(maximum*142/578*4)
-
-
-while disp.isNotDone():
-  if disp.mouseLeft:
-    break
+print("they are: " + str(rain1_blobs))
 
 
 
-#img1.sideBySide(rain1.image).show()
-#rain1.image.sideBySide(rain2.image).show()
+rain1.show()
+rain1.waitForClick(disp)
+
+rain2.show()
+rain2.waitForClick(disp)
+
+rain3.show()
+rain3.waitForClick(disp)
 
